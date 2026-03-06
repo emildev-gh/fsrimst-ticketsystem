ab7d73fa91ac40c18a31ad5c727e6b4dda3f1d92
Emil Laptop <emildev@magenta.de>
2026-03-06 21:04:21 +0100

    basic Footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,3 +1,11 @@
-<template></template>
+<template>
+  <footer class="text-primary-muted w-full pt-8 pb-8 text-center text-xs">
+    <Divider />
+    <slot />
+    <p>Noch Fragen? Kontaktieren Sie uns: <a href="">email@email.com</a></p>
+  </footer>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import Divider from '@/components/utility/Divider.vue'
+</script>
